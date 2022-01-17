90eb862ddfe9d77f216f9934688560d985361f64
josiahzhao <493106672@qq.com>
2022-01-18 00:51:37 +0800

    feat: update

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ git pull
 git add *
 git commit -m 'feat: update'
 git push origin master
-ssh mia "docker cp /data/code/articles/utils.js docker restart blog_docker-hexo-next_1:/hexo/website/themes/next/source/js/"
+ssh mia "docker cp /data/code/articles/utils.js blog_docker-hexo-next_1:/hexo/website/themes/next/source/js/"
 ssh mia "cd /data/code/articles && git pull && docker restart blog_docker-hexo-next_1"
